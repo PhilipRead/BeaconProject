0f6252d33a0d3c550aa96001bc3881929bbca2c4
PhilipRead <p647r602@ku.edu>
2014-09-17 14:21:41 -0500

    Modified code to only detect if entering or exiting beacon ranges. Device list added. Added About, Work Examples, Call, Email sections. Fixed Storyboard issues with updating to next version of Xcode 6. Added image icons. Copier device goes to google.com because a bug with UIWebView displaying PDF files is still unresolved by Apple.  -PR

---
+++ BeaconFoundation/BeaconFoundation/PhoneController.swift
@@ -0,0 +1,35 @@
+//
+//  PhoneController.swift
+//  BeaconFoundation
+//
+//  Created by Sarah Godke on 9/4/14.
+//  Copyright (c) 2014 GPW. All rights reserved.
+//
+
+import UIKit
+
+class PhoneController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
